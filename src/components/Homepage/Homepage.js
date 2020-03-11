4d65743021b34e068dcddb63be20d29863fee049
eryckalves <eryckalves@hotmail.com>
2020-03-11 15:47:07 -0300

    ver 0.4.0 uptade style homepage

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -3,7 +3,7 @@ import './Homepage.css';
 import {Grid , Cell } from 'react-mdl'
 import avatar from '../../images/eryckalves.jpg'; // Tell webpack this JS file uses this image
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookSquare , faReact, faHtml5 , faCss3 , faYoutubeSquare , faInstagram ,faTwitter, faLaravel } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookSquare , faReact, faHtml5 , faCss3 , faYoutubeSquare , faInstagram ,faTwitter, faLaravel ,faBootstrap } from '@fortawesome/free-brands-svg-icons';
 import Button from '@material-ui/core/Button';
 
 export class Homepage extends Component {
@@ -14,32 +14,34 @@ export class Homepage extends Component {
                     <Cell className="homepage-Cell" col={12}>
                         <div className="avatar-section-img">
                             <img col1="col1" className="avatar-img" src={avatar} alt=""/>
-                        </div>
-                        <div col2="col2" className="banner-text">
-                            <h1>Full Stack</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, voluptate.</p>
-                            <div className="avatar-section-icon">
-                                <div className="avatar-item-icon">
-                                    <FontAwesomeIcon className="avatar-icon" icon={faReact} /><span>ReactJS</span>
-                                </div>
-                                <div className="avatar-item-icon">
-                                    <FontAwesomeIcon className="avatar-icon" icon={faLaravel} /><span>Laravel</span>
-                                </div>
-                                <div className="avatar-item-icon">
-                                    <FontAwesomeIcon className="avatar-icon" icon={faHtml5} /><span>HTML</span>
-                                </div>
-                                <div className="avatar-item-icon">
-                                    <FontAwesomeIcon className="avatar-icon" icon={faCss3} /><span>CSS3</span>
-                                </div>
-                            </div>
-                            <div className="social-media">
-                                    <a href="/"><FontAwesomeIcon className="avatar-icon" icon={faFacebookSquare} /></a>
-                                    <a href="/"><FontAwesomeIcon className="avatar-icon" icon={faYoutubeSquare} /></a>
-                                    <a href="/"><FontAwesomeIcon className="avatar-icon" icon={faInstagram} /></a>
-                                    <a href="/"><FontAwesomeIcon className="avatar-icon" icon={faTwitter} /></a>
-                            </div>
-                        </div>                        
+                        </div>                       
                     </Cell>
+                    <div className="homepage-card">
+                        <Cell className="homepage-card-items" col={8} >
+                            <Cell className="homepage-card-item" col={4}>
+                                <h1>Language</h1>
+                                <a href="/"><FontAwesomeIcon className="homepage-card-item-icon" icon={faReact}/> ReactJS</a>
+                                <a href="/"><FontAwesomeIcon className="homepage-card-item-icon" icon={faLaravel}/> Laravel</a>
+                                <a href="/"><FontAwesomeIcon className="homepage-card-item-icon" icon={faBootstrap}/> Bootstrap4</a>
+                                <a href="/"><FontAwesomeIcon className="homepage-card-item-icon" icon={faCss3}/> CSS3</a>
+                                <a href="/"><FontAwesomeIcon className="homepage-card-item-icon" icon={faHtml5}/> HTML5</a>
+                                
+                            </Cell>
+                            <Cell className="homepage-card-item" col={4}>
+                                <h1>Social Media</h1>
+                                <a href="/"><FontAwesomeIcon className="homepage-card-item-icon" icon={faYoutubeSquare}/> Youtube</a>
+                                <a href="/"><FontAwesomeIcon className="homepage-card-item-icon" icon={faInstagram}/> Instagram</a>
+                                <a href="/"><FontAwesomeIcon className="homepage-card-item-icon" icon={faTwitter}/> Twitter</a>
+                                <a href="/"><FontAwesomeIcon className="homepage-card-item-icon" icon={faFacebookSquare}/> Facebook</a>                     
+                            </Cell>
+                            <Cell className="homepage-card-item" col={4}>
+                                <div className="">
+                                    <h1>FULLSTACK</h1>
+                                    <span>A full stack web developer is a person who can develop both client and server software.</span>
+                                </div>                       
+                            </Cell>
+                        </Cell>
+                    </div>
                     <Cell className="codes-section" col={12}>
                         <div>
                             <h1> Codes GibHub</h1>
